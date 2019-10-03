33b8329fa17d80a90f08d0f14fb9dd1df1174b6f
JuDelCo <judelco.dev@gmail.com>
2019-10-03 21:42:46 +0100

    Add extension helper methods to Context class.

--- Runtime/Context/IContextExtensions.cs
+++ Runtime/Context/IContextExtensions.cs
@@ -8,5 +8,25 @@ namespace Ju.ECS
 		{
 			return context.GetGroup(matcher).GetEntities();
 		}
+
+		public static IGroup GetGroup<T>(this IContext context) where T : IComponent
+		{
+			return context.GetGroup(MatcherGen.AllOf<T>());
+		}
+
+		public static IGroup GetGroup<T1, T2>(this IContext context) where T1 : IComponent where T2 : IComponent
+		{
+			return context.GetGroup(MatcherGen.AllOf<T1, T2>());
+		}
+
+		public static IGroup GetGroup<T1, T2, T3>(this IContext context) where T1 : IComponent where T2 : IComponent where T3 : IComponent
+		{
+			return context.GetGroup(MatcherGen.AllOf<T1, T2, T3>());
+		}
+
+		public static IGroup GetGroup<T1, T2, T3, T4>(this IContext context) where T1 : IComponent where T2 : IComponent where T3 : IComponent where T4 : IComponent
+		{
+			return context.GetGroup(MatcherGen.AllOf<T1, T2, T3>());
+		}
 	}
 }
